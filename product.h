febaee470ed0102396ab2617bb810f10a1d7fd6c
Logan Garcia <logangarcia@fastmail.fm>
2016-09-24 14:57:32 -0700

    added more methods and variables to product.h

--- product.h
+++ product.h
@@ -4,15 +4,19 @@
 class Product {
     private:
         char *name;     //name of the product
+        char *brand;    //brand of the product
+        char *dept;     //department the product is in
         int quantity;   //quantity of the product
         int msrp;       //retail price of the product
         int price;      //price of product (msrp - discount)
         int discount;   //discount of the product
         bool isDamaged; //true if product is damaged
     public:
-        Product(const char*, const char*, const char*, double, int);
+        Product(const char*, const char*, const char*, int, int);
         ~Product();
         char *getName() { return name; }
+        char *getBrand() { return brand; }
+        char *getDept() { return dept; }
         int getQuantity() { return quantity; }
         int getMsrp() { return msrp; }
         int getPrice() { return price; }
@@ -22,6 +26,7 @@ class Product {
         void setMsrp(int m) { msrp = m; }
         void setDiscount(int d) { discount = d; }
         void setDamage(bool b) { isDamaged = b; }
+        void display();
 };
 
 #endif
