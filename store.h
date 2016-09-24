39ad2bbaa93eba27f554d6860070bfa3b5faff22
Logan Garcia <logangarcia@fastmail.fm>
2016-09-24 14:55:23 -0700

    added more methods and variables to store.h

--- store.h
+++ store.h
@@ -2,23 +2,29 @@
 #define _STORE_H
 
 #include "employee.h"
+#include "inventory.h"
 #include <vector>
 
 class Store {
     private:
         std::vector<Employee*> employees;   //list of employees
+        Inventory *inventory;               //inventory for the store
         char *name;                         //name of store
         char *address;                      //current address of store
         int id;                             //id of store
+        void searchEmployee (const char*, char);
     public:
         Store(const char*, const char*, int);
         ~Store();
+        Inventory *getInventory() { return inventory; }
         char *getName() { return name; }
         char *getAddress() { return address }
         int getId() { return id; }
         void addEmployee(const char*, const char*, const char*, double, int);
-        void removeEmployee(const char*);
+        void removeEmployee(const char*, char);
         void changeEmployee(const char*, const char*, double);
+        void displayEmployee(const char*, char);
+        void displayEmployees();
         void setName(const char* n) { name = new char[strlen(n) + 1]; }
         void setAddress(const char* a) { address = new char[strlen(a) + 1]; }
         void setId(int i) { id = i }
